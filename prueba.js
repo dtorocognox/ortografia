e1799a91d82c10ac22259fd490f4369cf5b947a5
David Toro Alzate <dtoro@cognox.com>
2016-09-20 08:52:54 -0500

    Se agrega opción de cambio color de bordes cuando se acierta en la respuesta

--- prueba.js
+++ prueba.js
@@ -1,17 +1,47 @@
-var b;
-function inicio() {
+var wordX = ["arruga", "pelado", "hélice", "turbulencia"];
 
-    b = document.getElementById("boton");
-    b.addEventListener("click", calcular);
+function start() {
+    var button = document.getElementById("boton");
+    button.addEventListener("click", match);
 }
 
-function calcular() {
-    var a = document.getElementById("p1");
-    var palabra = a.value;
-    if(palabra == "diptongo"){
-        alert("genio");
+function match(){
+
+
+
+}
+
+function match() {
+
+    var singleWord, objectWord;
+    var index;
+
+    for(i=0; i<1; i++){
+        for (j=1; j < 5; j++){
+
+            index = "p" + i + j;
+
+            //obtiene caja de texto
+            objectWord = document.getElementById(index);
+
+            //ingresa palabra de la caja y la convierte en minuscula
+            singleWord = objectWord.value.toLowerCase();
+
+            if(wordX[j-1] == singleWord){
+                changeColor(objectWord, true);
+            }else{
+                changeColor(objectWord, false);
+            }
+        }
+    }
+}
+
+function changeColor(object, boolean){
+
+    if(boolean == true){
+        object.style.border="green 1px solid";
     }else{
-        alert("idioto");
-    };
+        object.style.border="red 1px solid";
+    }
 
 }
